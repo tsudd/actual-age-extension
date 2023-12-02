75c1843398d7ea9f373e8cdf76b1be4a82081b2e
Aliaksandr Khrystsenka <alex.kr1stehko@gmail.com>
2023-12-02 19:16:29 +0300

    upd: packages versions

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,17 +1,17 @@
-'use strict'
+"use strict";
 
-const { merge } = require('webpack-merge')
+const { merge } = require("webpack-merge");
 
-const common = require('./webpack.common.js')
-const PATHS = require('./paths')
+const common = require("./webpack.common.js");
+const PATHS = require("./paths");
 
 // Merge webpack configuration files
 const config = merge(common, {
   entry: {
-    tab: PATHS.src + '/apps/tab/tab.js',
-    background: PATHS.src + '/apps/background/background.js',
-    popup: PATHS.src + '/apps/popup/popup.js',
+    tab: PATHS.src + "/apps/tab/tab.js",
+    background: PATHS.src + "/apps/background/background.js",
+    popup: PATHS.src + "/apps/popup/popup.js",
   },
-})
+});
 
-module.exports = config
+module.exports = config;
